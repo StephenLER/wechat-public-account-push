8c68101aacdd6ea54f87176eea3528db94a4156e
StephenLER <102418198+StephenLER@users.noreply.github.com>
2024-04-12 00:11:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -5,7 +5,33 @@
  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
  * 自行添加属性，以支持更多个性化功能
  */
-const USER_CONFIG = {"APP_ID":"wxbb88a046d4aed3c3","APP_SECRET":"511642f628fe9ce506022619c030baaf","IS_SHOW_COLOR":false,"CALLBACK_TEMPLATE_ID":"wxbb88a046d4aed3c3","CALLBACK_USERS":[{"name":"自己","id":""}],"USERS":[{"name":"Lou","id":"o3HIM6lppOsx_Pw9ThAzi5LB-z7M","useTemplateId":"HkcbbDxsX7FRFRnL3nJRWBcfmtCvFBC_cHIKAKKPwSQ","province":"上海","city":"上海","horoscopeDate":"10-30","horoscopeDateType":"今日","openUrl":"https://shuangxunian.github.io/","festivals":[{"type":"生日","name":"birthday","date":"10-30","year":"2003"}],"customizedDateList":[]}],"SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":false,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":false,"birthdayMessage":true}}
+const USER_CONFIG = {
+  "APP_ID":"wxbb88a046d4aed3c3",
+  "APP_SECRET":"511642f628fe9ce506022619c030baaf",
+  "IS_SHOW_COLOR":false,
+  "CALLBACK_TEMPLATE_ID":"wxbb88a046d4aed3c3",
+  "CALLBACK_USERS":[{"name":"自己","id":""}],
+  "USERS":[
+    {"name":"Lou",
+     "id":"o3HIM6lppOsx_Pw9ThAzi5LB-z7M",
+     "useTemplateId":"HkcbbDxsX7FRFRnL3nJRWBcfmtCvFBC_cHIKAKKPwSQ",
+     "province":"上海","city":"上海",
+     "horoscopeDate":"10-30",
+     "horoscopeDateType":"今日",
+     "openUrl":"https://shuangxunian.github.io/",
+     "festivals":[{"type":"生日","name":"birthday","date":"10-30","year":"2003"}],
+     "customizedDateList":[]}],
+  "USERS":[
+    {"name":"Lou",
+     "id":"o3HIM6uPJWJ8hpQeohnQSzhcSwFU",
+     "useTemplateId":"HkcbbDxsX7FRFRnL3nJRWBcfmtCvFBC_cHIKAKKPwSQ",
+     "province":"上海","city":"上海",
+     "horoscopeDate":"10-30",
+     "horoscopeDateType":"今日",
+     "openUrl":"https://shuangxunian.github.io/",
+     "festivals":[{"type":"生日","name":"birthday","date":"10-30","year":"2003"}],
+     "customizedDateList":[]}],
+  "SWITCH":{"weather":true,"holidaytts":true,"CIBA":true,"oneTalk":false,"earthyLoveWords":false,"momentCopyrighting":false,"poisonChickenSoup":false,"poetry":false,"horoscope":false,"birthdayMessage":true}}
 
 module.exports = USER_CONFIG
 
